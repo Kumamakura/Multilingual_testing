c745ed2f10b427bef971bf5c8904e86f4e33f100
Kumamakura <zalender@gmail.com>
2020-08-10 00:31:15 +0500

    Убрала лишний print

--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ def test_guest_should_see_add_to_basket_button(browser):
     # даю возможность проверяющему посмотреть, какой язык у страницы
     time.sleep(5)
     assert browser.find_element_by_css_selector("button.btn-add-to-basket"), \
-        print('Кнопка "Добавить в корзину" отсутствует')
+        'Кнопка "Добавить в корзину" отсутствует'
